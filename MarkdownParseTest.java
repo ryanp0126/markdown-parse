177df8d75a0914a6ef8a7f408f1adc1e7e89afe4
ryanp0126 <ryanpuahzhiyuan@gmail.com>
2022-02-03 15:41:14 -0800

    fixed test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
-        assertEquals(2, 1-1);
+        assertEquals(2, 1 + 1);
     }
 
     @Test
