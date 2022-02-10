548f6e52f69d6469288a00d325d8d10f73a4e5bc
Ryan Z Puah <cs12wi22aie@ieng6-202.ucsd.edu>
2022-02-10 15:53:26 -0800

    added makefile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 
 //java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 
+//change
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
